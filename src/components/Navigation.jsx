01e1a3e7eff619250d14e7cfeec31c30b289a6b6
Jerpancho <panchojerrold@gmail.com>
2023-01-04 12:24:13 -0600

    added transitions to navbar scroll

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from '../css/Nav.module.css';
 const Navigation = () => {
    // use this ref to change the opacity of the navbar when scrolling
    const navRef = useRef();
+   const previousPos = useRef(window.scrollY);
+
+   const handleScroll = () => {
+      let currentPos = window.scrollY;
+      if (previousPos.current < currentPos) {
+         navRef.current.style.opacity = 0;
+         navRef.current.style.transform = 'translate(-50%, -300px)';
+      } else {
+         navRef.current.style.opacity = 1;
+         navRef.current.style.transform = 'translate(-50%, 1%)';
+      }
+      previousPos.current = currentPos;
+   };
+   useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      () => window.removeEventListener('scroll', handleScroll);
+   }, []);
    return (
       <nav className={styles.nav} ref={navRef}>
          <ul className={styles.list}>
